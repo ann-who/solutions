0e0f228c26b12f2093c6f244e4df0c123d949d17
Ann Impalovskaya <covertover@gmail.com>
2022-07-14 11:00:56 +0300

    Add conditional solutions

--- informatics/conditional/260_n_equation.dart
+++ informatics/conditional/260_n_equation.dart
@@ -7,13 +7,12 @@ void main() {
   int a = int.parse(stdin.readLineSync(encoding: utf8)!);
   int b = int.parse(stdin.readLineSync(encoding: utf8)!);
 
-  int x = -b ~/ a;
-
   if (a == 0 || b % a != 0) {
     stdout.write('NO');
   } else if (b == 0) {
     stdout.write('INF');
   } else {
+    int x = -b ~/ a;
     stdout.write(x);
   }
 }
